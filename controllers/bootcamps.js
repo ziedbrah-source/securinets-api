9a41e8a9c3a9844f271607bad08674be5564d289
Mohamed Zied Brahmi <ziedbrahmi2@gmail.com>
2021-11-21 20:51:10 +0100

    Add user model

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -60,12 +60,13 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @route Delete /api/v1/bootcamps/:id
 // @access Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
   if (!bootcamp) {
     next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+  bootcamp.remove();
   res.status(200).json({ success: true, data: {} });
 });
 
