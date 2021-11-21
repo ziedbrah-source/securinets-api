dd0e25563fda6031dfd0aeae901f8b68a97d375b
Mohamed Zied Brahmi <ziedbrahmi2@gmail.com>
2021-11-21 21:45:03 +0100

    Add login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const asyncHandler = require("../middleware/async");
 const ErrorResponse = require("../utils/errorResponse");
 
 // @desc Register user
-// @route Get /api/v1/auth/register
+// @route Post /api/v1/auth/register
 // @access Public
 
 exports.register = asyncHandler(async (req, res, next) => {
@@ -17,6 +17,37 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
   // Create token
+  const token = user.getSignedJwtToken();
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+// @desc Login user
+// @route Post /api/v1/auth/login
+// @access Public
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  // Validate email & password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and passowrd", 400));
+  }
+
+  // check for user
+  const user = await User.findOne({
+    email: email,
+  }).select("+password"); // because we made select: false in the model, so we add it here
+  // Create token
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  // check if password matchs
+  const isMatch = await user.matchPassword(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
   const token = user.getSignedJwtToken();
   res.status(200).json({
     success: true,
--- models/User.js
+++ models/User.js
@@ -46,4 +46,9 @@ UserSchema.methods.getSignedJwtToken = function () {
     expiresIn: process.env.JWT_EXPIRE,
   });
 };
+
+// Math user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 module.exports = mongoose.model("User", UserSchema);
