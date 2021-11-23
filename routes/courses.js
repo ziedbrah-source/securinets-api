caf25883033ebd7eaa216cd995743050eff88dfc
Mohamed Zied Brahmi <ziedbrahmi2@gmail.com>
2021-11-23 16:26:09 +0100

    Add CRUD for courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true }); // mergeParams let us use the routers in bootcamps router
-const { getCourses } = require("../controllers/courses");
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courses");
 router.get("/", getCourses);
+router.post("/", addCourse);
+router.post("/", addCourse);
+router.get("/:id", getCourse);
+router.put("/:id", updateCourse);
+router.delete("/:id", deleteCourse);
 
 module.exports = router;
