5b5b6ec04671de3f581952b014d796d24893b6e5
Mohamed Zied Brahmi <ziedbrahmi2@gmail.com>
2021-12-10 21:07:08 +0100

    Add users(for admin) route

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const errorHandler = require("./middleware/error");
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 const auth = require("./routes/auth");
+const users = require("./routes/users");
 
 // Connect to DB
 connectDB();
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
